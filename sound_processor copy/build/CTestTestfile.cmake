17769af633632260e41abbd1ef4339e2c6dc6db4
Matvej Plotnikov <madmplotnikov@edu.hse.ru>
2026-06-19 22:58:32 +0300

    done

--- sound_processor copy/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/plotnikovmatvej/se2510-Matvej-Plotnikov-madmplotnikov/tasks/sound_processor
-# Build directory: /Users/plotnikovmatvej/se2510-Matvej-Plotnikov-madmplotnikov/tasks/sound_processor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_sound_processor "/Users/plotnikovmatvej/se2510-Matvej-Plotnikov-madmplotnikov/tasks/sound_processor/build/test_sound_processor")
-set_tests_properties(test_sound_processor PROPERTIES  _BACKTRACE_TRIPLES "/Users/plotnikovmatvej/se2510-Matvej-Plotnikov-madmplotnikov/tasks/sound_processor/CMakeLists.txt;94;add_test;/Users/plotnikovmatvej/se2510-Matvej-Plotnikov-madmplotnikov/tasks/sound_processor/CMakeLists.txt;0;")
